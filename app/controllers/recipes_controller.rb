e8ba7be9a1cacf67a9a68c47192b371364a9ec13
Patrick McCombs <pa.mccombs1@gmail.com>
2019-06-04 21:32:57 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,6 +9,8 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: "first ingredient")
+    @recipe.ingredients.build(name: "second ingredient")
   end
 
   def create
